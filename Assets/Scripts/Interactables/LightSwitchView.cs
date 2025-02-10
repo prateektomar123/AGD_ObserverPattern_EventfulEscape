ed2b97cb4a675c718df91c875646ef25f2b48aa7
Prateek <prateektomar005@gmail.com>
2025-02-10 19:59:08 +0530

    Implemented generic Event Controller

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
-using UnityEditor.MPE;
 using UnityEngine;
-using static LightSwitchView;
 
 public class LightSwitchView : MonoBehaviour, IInteractable
 {
