7b0e1c9fd01ee52ba9895ad51055bc40210e4365
Prateek <prateektomar005@gmail.com>
2025-02-12 20:30:11 +0530

    Added listener in Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -28,12 +28,15 @@ public class PlayerController
 
         EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
         EventService.Instance.OnKeyPickedUp.AddListener(onKeysPickedUp);
+        EventService.Instance.OnLightsOffByGhostEvent.AddListener(onLightsTurnedOffByGhost);
     }
 
     ~PlayerController()
     {
         EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
         EventService.Instance.OnKeyPickedUp.RemoveListener(onKeysPickedUp);
+        EventService.Instance.OnLightsOffByGhostEvent.RemoveListener(onLightsTurnedOffByGhost);
+
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
@@ -87,6 +90,8 @@ public class PlayerController
         else
             PlayerState = PlayerState.InDark;
     }
+
+    private void onLightsTurnedOffByGhost() => PlayerState = PlayerState.InDark;
     private void onKeysPickedUp(int keys)
     {
         KeysEquipped = keys;
